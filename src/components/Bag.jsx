42eb780a0a9cd911627dfeefa52d2e7cbb407bc1
montenwav <157234524+montenwav@users.noreply.github.com>
2024-04-12 23:43:43 +0300

    fix bag

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useContext, useState } from "react"
-import { setIsBagOpenCtx } from '../hooks/Context.jsx'
+import { whyNotCardsCtx, setWhyNotCardsCtx, setIsBagOpenCtx } from '../hooks/Context.jsx'
 
 export function Bag({ bagRef, isBagOpen }) {
-    const [isEmpty, setIsEmpty] = useState(true)
+    const [isEmpty, setIsEmpty] = useState(false)
     const setIsBagOpen = useContext(setIsBagOpenCtx)
+
+    // Add button state
+    // const [addState, setAddState] = useState('none') // 'none', 'adding'
+    const [whyNotCards, setWhyNotCards] = useState(whyNotToAddArr)
 
     const freeShipping = 32;
 
@@ -18,12 +22,16 @@ export function Bag({ bagRef, isBagOpen }) {
     })
 
     return (
-        <div className="bag">
-            <div
-                onClick={() => setIsBagOpen(false)}
-                className="bag_bg"></div>
-            <BagContainer freeShipping={freeShipping} bagRef={bagRef} isEmpty={isEmpty} />
-        </div>
+        <whyNotCardsCtx.Provider value={whyNotCards}>
+            <setWhyNotCardsCtx.Provider value={setWhyNotCards}>
+                <div className="bag">
+                    <div
+                        onClick={() => setIsBagOpen(false)}
+                        className="bag_bg"></div>
+                    <BagContainer freeShipping={freeShipping} bagRef={bagRef} isEmpty={isEmpty} />
+                </div>
+            </setWhyNotCardsCtx.Provider>
+        </whyNotCardsCtx.Provider>
     )
 }
 
@@ -38,49 +46,52 @@ const BagContainer = ({ isEmpty, bagRef, freeShipping }) => {
     )
 }
 
-const BagTop = ({freeShipping}) => {
+const BagTop = ({ freeShipping }) => {
     const setIsBagOpen = useContext(setIsBagOpenCtx)
 
     return (
-        <div className="bag_top_bg">
-            <div className="bag_top">
-                <div className="bag_top_text">
-                    <h4>YOUR BAG</h4>
-                    {freeShipping <= 0 ? <h5>🎉 Congrats! You've Unlocked <b>FREE SHIPPING!</b></h5> : <h5>You Are ${freeShipping} Away From Free Shipping</h5>}
-                </div>
+        <>
+            <div className="bag_top_bg">
+                <div className="bag_top">
+                    <div className="bag_top_text">
+                        <h4>YOUR BAG</h4>
+                        {freeShipping <= 0 ? <h5>🎉 Congrats! You've Unlocked <b>FREE SHIPPING!</b></h5> : <h5>You Are ${freeShipping} Away From Free Shipping</h5>}
+                    </div>
 
-                <div className="bag_top_exit"
-                    onClick={() => setIsBagOpen(false)}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width={34}
-                        height={34}
-                        viewBox="0 0 34 34"
-                        fill="none"
+                    <div className="bag_top_exit"
+                        onClick={() => setIsBagOpen(false)}
                     >
-                        <rect x={1} y={1} width={32} height={32} fill="white" stroke="#131313" />{" "}
-                        <mask
-                            id="mask0_1986_1154"
-                            maskUnits="userSpaceOnUse"
-                            x={5}
-                            y={5}
-                            width={24}
-                            height={24}
-                            style={{ maskType: "alpha" }}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={34}
+                            height={34}
+                            viewBox="0 0 34 34"
+                            fill="none"
                         >
-                            <rect x={5} y={5} width={24} height={24} fill="#D9D9D9" />
-                        </mask>{" "}
-                        <g mask="url(#mask0_1986_1154)">
-                            <path
-                                d="M11.6732 22.6923L11 22.0191L16.3268 16.6923L11 11.3654L11.6732 10.6923L17 16.0191L22.3268 10.6923L23 11.3654L17.6732 16.6923L23 22.0191L22.3268 22.6923L17 17.3654L11.6732 22.6923Z"
-                                fill="#131313"
-                            />
-                        </g>
-                    </svg>
+                            <rect x={1} y={1} width={32} height={32} fill="white" stroke="#131313" />{" "}
+                            <mask
+                                id="mask0_1986_1154"
+                                maskUnits="userSpaceOnUse"
+                                x={5}
+                                y={5}
+                                width={24}
+                                height={24}
+                                style={{ maskType: "alpha" }}
+                            >
+                                <rect x={5} y={5} width={24} height={24} fill="#D9D9D9" />
+                            </mask>{" "}
+                            <g mask="url(#mask0_1986_1154)">
+                                <path
+                                    d="M11.6732 22.6923L11 22.0191L16.3268 16.6923L11 11.3654L11.6732 10.6923L17 16.0191L22.3268 10.6923L23 11.3654L17.6732 16.6923L23 22.0191L22.3268 22.6923L17 17.3654L11.6732 22.6923Z"
+                                    fill="#131313"
+                                />
+                            </g>
+                        </svg>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="hr" />
+        </>
     )
 }
 
@@ -93,11 +104,11 @@ const BagMiddle = ({ isEmpty }) => {
                         <h5>OOPS. YOUR BAG IS EMPTY.</h5>
                         <p>Here's what's trending:</p>
                     </div>
-                    <MiddleBagCard />
+                    <EmptyMiddleBagCard />
                 </div>
                 :
                 <div className="not_empty_bag">
-
+                    <MiddleBagCard />
                 </div>
             }
         </div>
@@ -105,6 +116,153 @@ const BagMiddle = ({ isEmpty }) => {
 }
 
 const MiddleBagCard = () => {
+    const addedItemsArr = [
+        {
+            src: 'https://cdn.shopify.com/s/files/1/1864/2187/files/pdp-hero-carousel-tpb-ff--4oz-mint_047543a9-2601-4cf1-a1c3-05686dd8f622_240x240.jpg?v=1702510991',
+            title: 'Toothpaste Bits',
+            color: 'Mint',
+            fluoride_free: true,
+            count: 1,
+            price: 32,
+            old_price: 48,
+        },
+    ]
+
+    const [addedItems, setAddedItems] = useState(addedItemsArr)
+
+    return (
+        <>
+            {addedItems.map((item, index) => (
+                <div key={index}>
+                    <div
+                        className="added_item_container">
+                        <div className="added_item">
+                            <div className="added_item_img">
+                                <img src={item.src} alt={item.title} />
+                            </div>
+
+                            <div className="added_item_description">
+                                <h1>{item.title}</h1>
+                                <h4>{item.color}</h4>
+
+                                <div className="delivers_every_bag">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width={22}
+                                        height={22}
+                                        viewBox="0 0 22 22"
+                                        fill="none"
+                                    >
+                                        <mask
+                                            id="mask0_1930_685"
+                                            maskUnits="userSpaceOnUse"
+                                            x={0}
+                                            y={0}
+                                            width={22}
+                                            height={22}
+                                            style={{ maskType: "alpha" }}
+                                        >
+                                            <rect width={22} height={22} fill="#D9D9D9" />
+                                        </mask>{" "}
+                                        <g mask="url(#mask0_1930_685)">
+                                            <path
+                                                d="M5.2637 13.6831C5.07801 13.2894 4.92582 12.8675 4.80713 12.4174C4.68844 11.9673 4.62909 11.4949 4.62909 11.0001C4.62909 9.21613 5.25782 7.69688 6.51529 6.44235C7.77278 5.18781 9.31348 4.58346 11.1374 4.62929H11.6927L10.1573 3.09385L11.0087 2.24243L13.9914 5.2251L11.0087 8.20778L10.1573 7.35636L11.6927 5.82091H11.1374C9.63665 5.79036 8.37535 6.28601 7.35349 7.30786C6.33164 8.32972 5.82071 9.56046 5.82071 11.0001C5.82071 11.3186 5.84686 11.6291 5.89915 11.9318C5.95145 12.2344 6.02989 12.5279 6.13449 12.8123L5.2637 13.6831ZM10.9911 19.7578L8.00843 16.7751L10.9911 13.7924L11.8425 14.6439L10.3071 16.1793H10.8624C12.3632 16.2099 13.6245 15.7142 14.6463 14.6923C15.6682 13.6705 16.1791 12.4397 16.1791 11.0001C16.1791 10.6816 16.1529 10.3711 16.1006 10.0685C16.0484 9.76584 15.9699 9.47233 15.8653 9.18792L16.7361 8.31711C16.9218 8.71081 17.074 9.1327 17.1927 9.5828C17.3114 10.0329 17.3707 10.5053 17.3707 11.0001C17.3707 12.7688 16.742 14.2842 15.4845 15.5464C14.227 16.8086 12.6863 17.4167 10.8624 17.3709H10.3071L11.8425 18.9064L10.9911 19.7578Z"
+                                                fill="#1C1B1F"
+                                            />
+                                        </g>
+                                    </svg>
+                                    <h5>Delivers every 4 months</h5>
+                                </div>
+
+                                <div className="counter_and_total">
+                                    <BagCounter item={item} />
+
+                                    <div className="bag_total">
+                                        <h4>${item.price}.00</h4>
+                                        <s style={{ color: 'gray' }}>
+                                            <h4>${item.old_price}.00</h4>
+                                        </s>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="hr" style={{ width: '95%' }}></div>
+                </div>
+            ))}
+        </>
+    )
+}
+
+const BagCounter = ({ item }) => {
+    return (
+        <div className="bag_counter">
+            <div className="bag_counter_flex">
+                <div className="bag_counter_item">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={18}
+                        height={18}
+                        viewBox="0 0 18 18"
+                        fill="none"
+                    >
+                        <mask
+                            id="mask0_1954_1480"
+                            maskUnits="userSpaceOnUse"
+                            x={0}
+                            y={0}
+                            width={18}
+                            height={18}
+                            style={{ maskType: "alpha" }}
+                        >
+                            <rect width={18} height={18} fill="#D9D9D9" />
+                        </mask>{" "}
+                        <g mask="url(#mask0_1954_1480)">
+                            <path d="M6.57703 10.3V9.69995H14.2514V10.3H6.57703Z" fill="#131313" />
+                        </g>
+                    </svg>
+                </div>
+
+                <div className="bag_counter_item">
+                    <h4>{item.count}</h4>
+                </div>
+
+                <div className="bag_counter_item">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={18}
+                        height={18}
+                        viewBox="0 0 18 18"
+                        fill="none"
+                    >
+                        <mask
+                            id="mask0_1954_1477"
+                            maskUnits="userSpaceOnUse"
+                            x={0}
+                            y={0}
+                            width={18}
+                            height={18}
+                            style={{ maskType: "alpha" }}
+                        >
+                            <rect width={18} height={18} fill="#D9D9D9" />
+                        </mask>{" "}
+                        <g mask="url(#mask0_1954_1477)">
+                            <path
+                                d="M3.36775 13.875V9.375H-0.818298V8.625H3.36775V4.125H4.06542V8.625H8.25147V9.375H4.06542V13.875H3.36775Z"
+                                fill="#131313"
+                            />
+                        </g>
+                    </svg>
+                </div>
+            </div>
+
+            <a href="">Remove</a>
+        </div>
+    )
+}
+
+const EmptyMiddleBagCard = () => {
     return (
         <>
             {middleBagArr.map((card, idx) => (
@@ -130,10 +288,11 @@ const WhyNotToAdd = () => {
     return (
         <>
             <div className="why_not_to_add">
-
+                <div className="hr"></div>
+                <Checkout />
                 <div className="hr"></div>
 
-                <h4>WHY NOT ADD?</h4>
+                <h4 className="why_not_to_add_h4">WHY NOT ADD?</h4>
                 <div className="why_not_to_add_flex">
                     <WhyNotToAddCard />
                 </div>
@@ -143,10 +302,51 @@ const WhyNotToAdd = () => {
     )
 }
 
-const WhyNotToAddCard = () => {
+const Checkout = () => {
+    const total = 0;
+
     return (
         <>
-            {whyNotToAddArr.map((card, idx) => (
+            <div className="checkout_container">
+                <div className="checkout">
+                    <h4>CHECKOUT</h4>
+                    <h4 className="bag_total">{`$${total}.00`}</h4>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const WhyNotToAddCard = () => {
+    const whyNotCards = useContext(whyNotCardsCtx)
+    const setWhyNotCards = useContext(setWhyNotCardsCtx)
+
+    const addingItem = (idx) => {
+
+        //Adding to the list
+
+
+        // Adding state
+        setWhyNotCards(whyNotToAddArr.map((card, index) => {
+            if (index === idx) {
+                return { ...card, added: true }
+            }
+            return card
+        }))
+
+        setTimeout(() => {
+            setWhyNotCards(whyNotToAddArr.map((card, index) => {
+                if (index == idx) {
+                    return { ...card, added: false }
+                }
+                return card
+            }))
+        }, 1000)
+    }
+
+    return (
+        <>
+            {whyNotCards.map((card, idx) => (
                 <div
                     key={idx}
                     className="why_not_to_add_card"
@@ -159,7 +359,12 @@ const WhyNotToAddCard = () => {
                         <h2>{card.title}</h2>
                     </div>
 
-                    <a href=""><button className="why_not_to_add_button">ADD</button></a>
+                    <button
+                        onClick={() => addingItem(idx)}
+                        disabled={card.added}
+                        style={{ background: card.added && 'gray' }}
+                        className="why_not_to_add_button"
+                    >{card.added ? 'ADDING...' : 'ADD'}</button>
                 </div>
             ))}
         </>
@@ -186,18 +391,26 @@ const middleBagArr = [
 
 const whyNotToAddArr = [
     {
+        id: 0,
+        added: false,
         title: 'Bamboo Toothbrush Subscription',
         src: 'https://cdn.shopify.com/s/files/1/1864/2187/files/pdp-hero-carousel-desktop-toothbrush_49ff32a7-1a50-4b23-98bf-58bdaca21188_400x400.jpg?v=1704393857',
     },
     {
+        id: 1,
+        added: false,
         title: 'Fluoride-Free Toothpaste Subscriptio',
         src: 'https://cdn.shopify.com/s/files/1/1864/2187/files/pdp-hero-carousel-tpb-ff--4oz-mint_047543a9-2601-4cf1-a1c3-05686dd8f622_400x400.jpg?v=1702510991'
     },
     {
+        id: 2,
+        added: false,
         title: 'Whitening Gel',
         src: 'https://cdn.shopify.com/s/files/1/1864/2187/files/pdp-hero-carousel-desktop-whitening-gel_400x400.jpg?v=1700142723',
     },
     {
+        id: 3,
+        added: false,
         title: 'Fluoride Toothpaste Subscription',
         src: 'https://cdn.shopify.com/s/files/1/1864/2187/files/pdp-hero-carousel-tpb-wf-2oz-mint-fluoride_1cdc3501-5ff2-4aed-9d5b-514ed12dd018_400x400.jpg?v=1704318283',
     },
--- src/hooks/Context.jsx
+++ src/hooks/Context.jsx
@@ -5,3 +5,6 @@ export const setIsBagOpenCtx = createContext(null)
 
 export const isAdaptiveCtx = createContext(null)
 export const setIsAdaptiveCtx = createContext(null)
+
+export const whyNotCardsCtx = createContext(null)
+export const setWhyNotCardsCtx = createContext(null)
